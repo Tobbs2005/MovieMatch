8ba75f5d437d855616c6cd0eea7ccd0a1d158d54
Toby Fang <tobyfang05@gmail.com>
2025-08-08 17:56:42 +0800

    Added analytics to vercel

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Geist, Geist_Mono } from "next/font/google";
 import { ThemeProvider } from "../components/ThemeProvider";
 import "./globals.css";
 
+import { Analytics } from "@vercel/analytics/next";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -31,6 +33,7 @@ export default function RootLayout({
         <ThemeProvider defaultTheme="system" storageKey="movie-match-theme">
           {children}
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   );
